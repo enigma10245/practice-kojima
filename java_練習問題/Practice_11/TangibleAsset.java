e52a0fe83746562aa418cb125e562a437037d12d
enigma <the.world.truth3616@gmail.com>
2021-02-04 16:56:33 +0900

    add:Practice_11を追加

---
+++ java_練習問題/Practice_11/TangibleAsset.java
@@ -0,0 +1,21 @@
+public abstract class TangibleAsset extends Asset implements Thing{
+    String color;
+
+    double weight;
+    public TangibleAsset(String name, int price, String color) {
+        super(name, price);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
